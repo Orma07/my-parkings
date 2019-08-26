bf524ff77e50d60788f129b3eea7c106e6d411a3
Amro <amro.abdelgawwad@gmail.com>
2019-08-26 10:51:51 +0200

    * bottom-navigation.Forms.csproj: IconView implemented
    * my-parkings.Forms.csproj:
    * my-parkings.iOS.csproj:
    * my-parkings.Core.csproj:
    * my-parkings.Android.csproj:
    * IconView.cs:
    * MainPage.xaml:
    * icon-view.Forms.csproj:

---
+++ bottom-navigation.Forms/icon-view.Forms/IconView.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace iconview.Forms
+{
+    public class IconView : SKCanvasView
+    {
+     
+
+        #region LineColor
+        public static readonly BindableProperty LineColorProperty =
+        BindableProperty.Create("LineColor",
+            typeof(Color),
+            typeof(IconView),
+            (Color)Application.Current.Resources["lineColor"],
+            propertyChanged: OnLineChange);
+
+
+
+        private static void OnLineChange(BindableObject bindable, object oldValue, object newValue)
+        {
+            var current = (IconView)bindable;
+            current.InvalidateSurface();
+
+
+        }
+
+        public Color LineColor
+        {
+            get { return (Color)GetValue(LineColorProperty); }
+            set { SetValue(LineColorProperty, value); }
+        }
+        #endregion
+
+        #region LineWidth
+        public static readonly BindableProperty LineWidthProperty =
+        BindableProperty.Create("LineWidth",
+            typeof(float),
+            typeof(IconView),
+            1f,
+            propertyChanged: OnLineChange);
+
+
+        public float LineWidth
+        {
+            get { return (float)GetValue(LineWidthProperty); }
+            set { SetValue(LineWidthProperty, value); }
+        }
+        #endregion
+
+        #region Source
+        public static readonly BindableProperty SourceProperty =
+        BindableProperty.Create("Source",
+            typeof(string),
+            typeof(IconView),
+            "",
+            propertyChanged: OnLineChange);
+
+
+        /// <summary>
+        /// Add full path of resource, including also the project folder, and the extension of svg file
+        /// </summary>
+        public string Source
+        {
+            get { return (string)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+        #endregion
+
+        protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+        {
+            base.OnPaintSurface(e);
+            SKImageInfo info = e.Info;
+            SKSurface surface = e.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            var strokeWidth = (float)(info.Height / Height) * LineWidth;
+
+            canvas.Clear();
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                StrokeCap = SKStrokeCap.Square,
+                Color = LineColor.ToSKColor(),
+                StrokeWidth = strokeWidth,
+                IsDither = true,
+                IsAntialias = true
+            };
+
+            var svg = new SkiaSharp.Extended.Svg.SKSvg();
+
+            var type = typeof(IconView).GetTypeInfo();
+            var assembly = type.Assembly;
+            canvas.Translate(info.Width / 2f, info.Height / 2f);
+
+
+            try
+            {
+                using (paint)
+                {
+                    using (var s = assembly.GetManifestResourceStream(Source))
+                        svg.Load(s);
+                    var svgSize = svg.Picture.CullRect;
+                    float svgMax = Math.Max(svgSize.Width, svgSize.Height);
+                    paint.ColorFilter = SKColorFilter.CreateBlendMode(LineColor.ToSKColor(), SKBlendMode.SrcIn);
+
+                    // calculate the scaling need to fit
+                    float canvasMin = Math.Min(info.Width, info.Height);
+                    float scale = (canvasMin / svgMax) * 0.7f;
+                    var matrix = SKMatrix.MakeScale(scale, scale);
+                    matrix.TransX = -scale * svg.Picture.CullRect.Width / 2;
+
+                    matrix.TransY = -(scale * svg.Picture.CullRect.Height / 2);
+
+                    // draw the svg
+                    canvas.DrawPicture(svg.Picture, ref matrix, paint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"IconView: couldn't draw svg ex - {ex}");
+            }
+
+        }
+      
+    }
+
+    
+}
